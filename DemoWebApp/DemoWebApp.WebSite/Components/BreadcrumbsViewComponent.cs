f32bce6b90358d34269a130898e00cd75f148b00
Ihar Slaunikau <Ihar_Slaunikau@epam.com>
2023-12-04 00:34:51 +0300

    Task 3.2 fix issues

--- DemoWebApp/DemoWebApp.WebSite/Components/BreadcrumbsViewComponent.cs
+++ DemoWebApp/DemoWebApp.WebSite/Components/BreadcrumbsViewComponent.cs
@@ -3,10 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DemoWebApp.WebSite.Components;
 
+[ViewComponent(Name = "Breadcrumb")]
 public class BreadcrumbsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(IEnumerable<BreadcrumbModel> items)
+    public IViewComponentResult Invoke()
     {
-        return View(items);
+        var currentController = ViewContext.RouteData.Values["controller"].ToString();
+        var currentAction = ViewContext.RouteData.Values["action"].ToString();
+
+        var baseUrl = $"{Url.ActionContext.HttpContext.Request.Scheme}://{Url.ActionContext.HttpContext.Request.Host}";
+
+        var breadcrumbs = new List<BreadcrumbModel>
+        {
+            new BreadcrumbModel { Label = "Home", Url = new Uri($"{baseUrl}{Url.Action("Index", "Home")}") },
+            new BreadcrumbModel { Label = currentController, Url = new Uri($"{baseUrl}{Url.Action("Index", currentController)}") },
+            new BreadcrumbModel { Label = currentAction }
+        };
+
+        return View(breadcrumbs);
     }
 }
